96b2038351711c3634279252899d80081e8afd83
Félix VILLEMIN <fvillemin@iliad-free.fr>
2025-09-22 15:03:03 +0200

    feat: add mimi_qwerty

---
+++ keyboards/cornifi/keymaps/mimi_qwerty/config.h
@@ -0,0 +1,7 @@
+// Copyright 2025 @v3lmx
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+#pragma once
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
